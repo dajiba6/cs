c485aff01fd2ec29ee02d09a270989befc9129da
chaoyining <chao.yining@wd.com>
2023-07-05 10:07:10 +0800

    money count script

---
+++ cs61a/money_count.py
@@ -0,0 +1,23 @@
+def fuli2(start, goal, income, rate, count=0):
+    print("cash :", start, "years :", count / 12)
+    if start >= goal:
+        return count
+    return fuli2(start * rate + income, goal, income, rate, count + 1)
+
+
+start = 10000
+income = 6000
+rate = 1.01
+goal = 300000
+
+print(
+    "\n",
+    "start = ",
+    start,
+    "\n",
+    "months needed = ",
+    fuli2(start, goal, income, rate),
+    "\n",
+    "years needed = ",
+    fuli2(start, goal, income, rate) / 12,
+)
