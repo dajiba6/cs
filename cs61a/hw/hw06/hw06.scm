08dd195198ed3bf48f66d9d4ec925b60cd15f646
cyn <chao.yn@foxmail.com>
2023-10-13 18:08:55 +0800

    hw06 done

--- cs61a/hw/hw06/hw06.scm
+++ cs61a/hw/hw06/hw06.scm
@@ -1,11 +1,29 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) 
+(car(cdr s))
+)
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) 
+(car (cddr s))
+)
 
-(define (sign val) 'YOUR-CODE-HERE)
+(define (sign val) 
+(cond
+    ((< val 0)-1)
+    ((> val 0)1)
+    ((= val 0)0)
+)
+)
 
 (define (square x) (* x x))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+    (cond
+    ((= exp 0) 1)
+    ((= exp 1) base)
+    ((even? exp) (square (pow base (/ exp 2))))
+    (else (* base (square (pow base (/ (- exp 1) 2)))))
+    )
+
+)
