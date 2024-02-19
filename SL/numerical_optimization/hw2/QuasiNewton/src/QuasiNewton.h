4e464a6385472e78f3eaba16ddee41c1cd2d2bb4
cyn <chao.yn@foxmail.com>
2024-02-19 18:06:35 +0800

    LBFGS method wrong fuck

--- SL/numerical_optimization/hw2/QuasiNewton/src/QuasiNewton.h
+++ SL/numerical_optimization/hw2/QuasiNewton/src/QuasiNewton.h
@@ -35,25 +35,30 @@ public:
     VectorXd gradient_value = traget_gradient_function_(x);
     double c1 = 1e-4;
     double c2 = 0.9;
-    double alpha = 0.1;
+    double alpha = 20;
     while (cost_value - target_cost_function_(x + alpha * direction) <
                -c1 * alpha * direction.transpose() * gradient_value ||
            direction.transpose() * traget_gradient_function_(x + alpha * direction) <
                c2 * direction.transpose() * gradient_value)
     {
       alpha *= 0.5;
+      // cout << "cost: " << target_cost_function_(x + alpha * direction) << endl;
+      bool con1 = (cost_value - target_cost_function_(x + alpha * direction) < -c1 * alpha * direction.transpose() * gradient_value);
+      bool con2 = direction.transpose() * traget_gradient_function_(x + alpha * direction) <
+                  c2 * direction.transpose() * gradient_value;
+      // cout << "con1: " << con1 << " con2: " << con2 << endl;
     }
 
     //! 步长test
-    // cout << "little test: " << alpha << endl;
-    cout << "difference:" << cost_value - target_cost_function_(x + alpha * direction) << endl;
-    cout << "left       " << -c1 * alpha * direction.transpose() * gradient_value << endl;
+    // cout << "difference:" << cost_value - target_cost_function_(x + alpha * direction) << endl;
+    // cout << "left       " << -c1 * alpha * direction.transpose() * gradient_value << endl;
     // cout << "gradient_value" << endl
     //      << gradient_value << endl;
     // cout << "direction" << endl
     //      << direction << endl;
     // cout << "alpha: " << alpha << endl;
-    // cout << "cost: " << target_cost_function_(x + alpha * direction) << endl;
+    cout << "new cost: " << target_cost_function_(x + alpha * direction) << endl;
+    // cout << "old cost: " << cost_value << endl;
     return alpha;
   }
 
@@ -80,6 +85,23 @@ public:
       beta = lbfgs_rho_vec[i] * lbfgs_y_vec[i].transpose() * d;
       d = d + lbfgs_s_vec[i] * (lbfgs_alpha_vec[i] - beta);
     }
+
+    /* TODO LBFGS direction result wrong: lbfgs_s_vec goes 0 -nan -> lbfgs_rho_vec[current_length - 1] goes inf -nan -> gamma goes -nan -> d goes -nan
+       x 错误移动导致 g 没改变 导致 rho求解爆炸
+       第三次循环时x移动错误
+       第三次移动由LBFGS决定方向，wolfe决定步长
+       第三次步长移动了50，但x没改变多少，方向没有特别奇怪的数字
+       步长中的 new cost 第二次和第四次数值无变化
+
+    */
+
+    // cout << "LBFGS_direction: " << endl
+    //  << d << endl;
+    // cout << "gamma: " << gamma << endl;
+    // cout << "lbfgs_rho_vec[current_length - 1]: " << lbfgs_rho_vec[current_length - 1] << endl;
+    // cout << "lbfgs_s_vec: " << lbfgs_s_vec[current_length - 1] << endl;
+    // cout << "lbfgs_y_vec: " << lbfgs_y_vec[current_length - 1] << endl;
+    // cout << "part of rho: " << lbfgs_s_vec[current_length - 1].transpose() * lbfgs_y_vec[current_length - 1] << endl;
     return d;
   }
 
@@ -124,10 +146,19 @@ public:
         lbfgs_rho_vec.pop_front();
       }
 
+      // 把数据输入到文件中画图
+      for (int i = 0; i < x.size(); ++i)
+      {
+        outputFile << x[i] << " ";
+      }
+      outputFile << "\n";
+
       direction = LBFGS(new_g);
 
       x = new_x;
       g = new_g;
+
+      // cout << count << " iteration: " << target_cost_function_(x) << endl;
       count++;
     }
     // 把数据输入到文件中画图
