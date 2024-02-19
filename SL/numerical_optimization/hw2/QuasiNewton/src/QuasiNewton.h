db2b8bd9fd074e335f151e23d49ce10e680dc7ab
cyn <chao.yn@foxmail.com>
2024-02-19 13:39:52 +0800

    vscode save sluggishly due to git plugin

--- SL/numerical_optimization/hw2/QuasiNewton/src/QuasiNewton.h
+++ SL/numerical_optimization/hw2/QuasiNewton/src/QuasiNewton.h
@@ -5,22 +5,22 @@
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/LU>
 #include <fstream>
+#include <deque>
 
 using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-typedef VectorXd (*gradient)(VectorXd input_data);
-typedef double (*func)(VectorXd input_data);
-
 class QuasiNewton
 {
 public:
-  QuasiNewton(func cost_func, gradient gradient, VectorXd input_data) : target_cost_function_(cost_func),
-                                                                        traget_gradient_function_(gradient),
-                                                                        init_x_(input_data),
-                                                                        lbfgs_limit_(30),
-                                                                        lbfgs_alpha_vec(lbfgs_limit_, 0.0)
+  QuasiNewton(std::function<double(Eigen::VectorXd)> cost_func,
+              std::function<Eigen::VectorXd(Eigen::VectorXd)> gradient,
+              VectorXd input_data) : target_cost_function_(cost_func),
+                                     traget_gradient_function_(gradient),
+                                     init_x_(input_data),
+                                     lbfgs_limit_(30),
+                                     lbfgs_alpha_vec(lbfgs_limit_, 0.0)
   {
     cout << "Initialization complete." << endl;
   }
@@ -50,19 +50,21 @@ public:
    * @brief use L-BFGS method to find B
    * @return {*}
    */
-  MatrixXd LBFGS(VectorXd gradient)
+  // todo: lbfgs当数组只有1个数据时的处理方法
+  VectorXd LBFGS(VectorXd gradient)
   {
     VectorXd d = gradient;
-    for (int i = 0; i < lbfgs_limit_; i++)
+    int current_length = std::min(lbfgs_limit_, static_cast<int>(lbfgs_s_vec.size()));
+    for (int i = 0; i < current_length; i++)
     {
       lbfgs_alpha_vec[i] = lbfgs_rho_vec[i] * lbfgs_y_vec[i].transpose() * lbfgs_s_vec[i];
       d = d - lbfgs_alpha_vec[i] * lbfgs_y_vec[i];
     }
-    double gamma = lbfgs_rho_vec[lbfgs_limit_ - 2] *
-                   lbfgs_y_vec[lbfgs_limit_ - 2].transpose() * lbfgs_y_vec[lbfgs_limit_ - 2];
+    double gamma = lbfgs_rho_vec[current_length - 2] *
+                   lbfgs_y_vec[current_length - 2].transpose() * lbfgs_y_vec[current_length - 2];
     d = d / gamma;
     double beta = 0;
-    for (int i = 0; i < lbfgs_limit_; i++)
+    for (int i = 0; i < current_length; i++)
     {
       beta = lbfgs_rho_vec[i] * lbfgs_y_vec[i].transpose() * d;
       d = d + lbfgs_s_vec[i] * (lbfgs_alpha_vec[i] - beta);
@@ -79,16 +81,16 @@ public:
     VectorXd x = init_x_;
     VectorXd g;
     MatrixXd B;
-    int count;
+    int count = 0;
     VectorXd result;
+    g = traget_gradient_function_(x);
+    VectorXd direction = g;
 
     while (g.cwiseAbs().maxCoeff() > 1e-6)
     {
-      VectorXd direction = -B * g;
       double t = LinsearchWeakWolfe(direction, x);
       VectorXd new_x = x + t * direction;
       VectorXd new_g = traget_gradient_function_(new_x);
-
       // 存入历史数据供LBFGS计算
       lbfgs_s_vec.push_back(new_x - x);
       lbfgs_y_vec.push_back(new_g - g);
@@ -96,29 +98,40 @@ public:
           .push_back(1.0 / ((new_x - x).transpose() * (new_g - g)));
       if (lbfgs_s_vec.size() > lbfgs_limit_)
       {
-        lbfgs_s_vec.erase(lbfgs_s_vec.begin());
-        lbfgs_y_vec.erase(lbfgs_y_vec.begin());
-        lbfgs_rho_vec.erase(lbfgs_rho_vec.begin());
+        lbfgs_s_vec.pop_front();
+        lbfgs_y_vec.pop_front();
+        lbfgs_rho_vec.pop_front();
       }
 
-      B = LBFGS(new_g);
+      direction = LBFGS(new_g);
       x = new_x;
       g = new_g;
       count++;
     }
+    iteration_times_ = count;
     final_value_ = target_cost_function_(x);
     final_x = x;
   }
 
+  void ResultInfo()
+  {
+    cout << "======== result =========" << endl;
+    cout << "iteration times: " << iteration_times_ << endl;
+    cout << "final_value_: " << final_value_ << endl;
+    cout << "final_x:" << final_x << endl;
+  }
+
+  int lbfgs_limit_;
+
 private:
   VectorXd init_x_;
-  func target_cost_function_;
-  gradient traget_gradient_function_;
+  std::function<double(Eigen::VectorXd)> target_cost_function_;
+  std::function<Eigen::VectorXd(Eigen::VectorXd)> traget_gradient_function_;
   double final_value_;
   VectorXd final_x;
-  int lbfgs_limit_ = 30;
-  vector<VectorXd> lbfgs_s_vec;
-  vector<VectorXd> lbfgs_y_vec;
-  vector<double> lbfgs_rho_vec;
+  deque<VectorXd> lbfgs_s_vec;
+  deque<VectorXd> lbfgs_y_vec;
+  deque<double> lbfgs_rho_vec;
   vector<double> lbfgs_alpha_vec;
+  int iteration_times_ = 0;
 };
